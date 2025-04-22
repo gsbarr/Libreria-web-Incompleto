12bfcb1e372c3bd6b8e46571aca04466f23489eb
gsbarr <gabriel.s.barreiro@gmail.com>
2025-04-21 21:47:34 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -14,35 +14,84 @@ class Libro {
 }
 
 let productosEnVenta = [];
+let productosCarrito = [];
 
 function nuevoProd(){
-    let nombreProd = document.querySelector("#producto");
+    let nombreProd = document.querySelector("#producto"); // es como CSS
     let stockProd = document.querySelector("#stock");
 
     let nuevoLibro = new Libro(nombreProd.value, stockProd.value);
     // Agregamos el nuevo libro a la lista
     productosEnVenta.push(nuevoLibro);
 
-   console.log(productosEnVenta);
+    console.log(productosEnVenta);
     cargarProd();
+}
+ 
+function agregarListaCarrrito(){
+
+    let nombreProd = document.querySelector("#productoVenta"); 
+    let stockProd = document.querySelector("#stockVenta");
+
+    let nuevoLibro = new Libro(nombreProd.value, stockProd.value);
+    // Agregamos el nuevo libro a la lista
+    productosCarrito.push(nuevoLibro);
+
+    console.log(productosCarrito);
+    cargarCarrito();
+
 }
 
 function cargarProd() {
     let listaVenta = document.querySelector(".formu_listaProd");
 
-    let plantillaVenta = ` <div class="input-field col s6">
-                <input placeholder="Producto" id="producto" type="text"  value="${productosEnVenta[0].nombre}" disabled>
-                <label for="producto">Nombre del producto</label>
-            </div>
-            <div class="input-field col s3">
-                <input placeholder="Stock" id="stock" type="text" disabled value="${productosEnVenta[0].stock}">
-                <label for="stock">Stock</label>
-            </div>
-            <div class="col s3">
-                <br>
-                <a class="waves-effect waves-light btn">Agregar 1 a carrito</a>
-            </div>
-            `;
+    listaVenta.innerHTML = "";
 
-    listaVenta.innerHTML += plantillaVenta;
+    productosEnVenta.forEach((libro) => {
+
+        let plantillaVenta = ` <div class="input-field col s6">
+            <input placeholder="Producto" id="productoVenta" type="text"  value="${libro.nombre}" disabled>
+            
+        </div>
+        <div class="input-field col s3">
+            <input placeholder="Stock" id="stockVenta" type="text" disabled value="${libro.stock}">
+            
+        </div>
+        <div class="col s3">
+            <br>
+            <a class="waves-effect waves-light btn" onclick="">Eliminar</a>
+        </div>
+        `;
+
+            listaVenta.innerHTML += plantillaVenta;
+
+    });
+   
+}
+
+function cargarCarrito() {
+    let listCarrito = document.querySelector(".formu_listaCarrito");
+
+    listCarrito.innerHTML = "";
+
+    productosCarrito.forEach((libro) => {
+
+        let plantillaVenta = ` <div class="input-field col s6">
+            <input placeholder="Producto" id="productoVenta" type="text"  value="${libro.nombre}" disabled>
+            
+        </div>
+        <div class="input-field col s3">
+            <input placeholder="Stock" id="stockVenta" type="text" disabled value="${libro.stock}">
+            
+        </div>
+        <div class="col s3">
+            <br>
+            <a class="waves-effect waves-light btn" onclick="agregarListaCarrrito()">Agregar 1 a carrito</a>
+        </div>
+        `;
+
+        listCarrito.innerHTML += plantillaVenta;
+
+    });
+   
 }
